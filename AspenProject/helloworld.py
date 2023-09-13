cba0b618d8720b725cfa39bc4e958dbc32660324
ZiXuan_Zhang <871523104@qq.com>
2023-09-13 10:04:47 +0800

    可移动窗口，可显示输入页面

--- AspenProject/helloworld.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-fpath = "F:\AspenTest\耦合工艺20230413\Result1"
-result = pd.read_excel(fpath)
---
+++ AspenProject/main.py
@@ -0,0 +1,40 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from untitled1 import *
+
+
+class TestWidget(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.ui = Ui_MainWindow_inter()
+        self.ui.setupUi(self)
+        self.setWindowFlag(Qt.FramelessWindowHint)
+        self.setAttribute(Qt.WA_TranslucentBackground)
+        self.show()
+
+    def mouseMoveEvent(self, e: QMouseEvent):  # 重写移动事件
+        if self._tracking:
+            self._endPos = e.pos() - self._startPos
+            self.move(self.pos() + self._endPos)
+
+    def mousePressEvent(self, e: QMouseEvent):
+        if e.button() == Qt.LeftButton:
+            self._startPos = QPoint(e.x(), e.y())
+            self._tracking = True
+
+    def mouseReleaseEvent(self, e: QMouseEvent):
+        if e.button() == Qt.LeftButton:
+            self._tracking = False
+            self._startPos = None
+            self._endPos = None
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    game = TestWidget()
+    game.show()
+    sys.exit(app.exec_())
